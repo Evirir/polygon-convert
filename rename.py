8665f3628d32d4acd33b40f6471cec667f57e40f
Evirir <gojunxing@gmail.com>
2025-04-20 02:33:33 +0800

    Linting

--- rename.py
+++ rename.py
@@ -1,24 +1,23 @@
 """Creates a folder and zip file containing tests in CMS format and
 outputs the Score Parameters string."""
+
 import json
 import os
 import shutil
 import sys
 import xml.etree.ElementTree as ET
 from string import Template
-from typing import Dict, List, Set
+
+OLD_TESTS_DIR = "tests"
+NEW_TESTS_DIR = "cms_tests"
+POLYGON_INPUT_TEMPLATE = Template("$id")
+POLYGON_OUTPUT_TEMPLATE = Template("$id.a")
+CMS_INPUT_TEMPLATE = Template("input.${id}_$group")
+CMS_OUTPUT_TEMPLATE = Template("output.${id}_$group")
+GROUPS_REGEX = Template(".*_(?:$groups)")
 
 
-OLD_TESTS_DIR = 'tests'
-NEW_TESTS_DIR = 'cms_tests'
-POLYGON_INPUT_TEMPLATE = Template('$id')
-POLYGON_OUTPUT_TEMPLATE = Template('$id.a')
-CMS_INPUT_TEMPLATE = Template('input.${id}_$group')
-CMS_OUTPUT_TEMPLATE = Template('output.${id}_$group')
-GROUPS_REGEX = Template('.*_(?:$groups)')
-
-
-def dfs(dependencies: Dict[str, Set[str]], visited: Set[str], group: str) -> None:
+def dfs(dependencies: dict[str, set[str]], visited: set[str], group: str) -> None:
     """Helper function for copy_children_prereqs."""
     if group in visited:
         return
@@ -31,25 +30,25 @@ def dfs(dependencies: Dict[str, Set[str]], visited: Set[str], group: str) -> Non
     dependencies[group] = new_prereqs
 
 
-def copy_children_prereqs(dependencies: Dict[str, Set[str]]):
+def copy_children_prereqs(dependencies: dict[str, set[str]]):
     """Add all dependencies of children recursively."""
     visited = set()
     for group in dependencies:
         dfs(dependencies, visited, group)
 
 
-def parse_dependencies(groups: List[ET.Element]) -> Dict[str, List[str]]:
+def parse_dependencies(groups: list[ET.Element]) -> dict[str, list[str]]:
     """Returns a dictionary mapping group names to prerequisites."""
-    dependencies: Dict[str, Set[str]] = {}
+    dependencies: dict[str, set[str]] = {}
 
     for group in groups:
-        name = group.get('name')
-        prereqs = group.find('dependencies')
+        name = group.get("name")
+        prereqs = group.find("dependencies")
         if prereqs is not None:
-            prereqs = prereqs.findall('dependency')
+            prereqs = prereqs.findall("dependency")
         else:
             prereqs = []
-        dependencies[name] = {prereq.get('group') for prereq in prereqs}
+        dependencies[name] = {prereq.get("group") for prereq in prereqs}
 
     copy_children_prereqs(dependencies)
 
@@ -58,7 +57,7 @@ def parse_dependencies(groups: List[ET.Element]) -> Dict[str, List[str]]:
     return dependencies
 
 
-def rename_tests(tests: List[ET.Element]):
+def rename_tests(tests: list[ET.Element]):
     """Create a copy of tests from Polygon and rename them to CMS format with groups appended,
     in a zip file."""
 
@@ -70,32 +69,42 @@ def rename_tests(tests: List[ET.Element]):
     width = len(str(len(tests)))
     for test_id, test in enumerate(tests):
         test_id_str = str(test_id + 1).zfill(width)
-        group = test.get('group')
+        group = test.get("group")
 
         polygon_input_name = os.path.join(
-            NEW_TESTS_DIR, POLYGON_INPUT_TEMPLATE.substitute(id=test_id_str, group=group))
+            NEW_TESTS_DIR,
+            POLYGON_INPUT_TEMPLATE.substitute(id=test_id_str, group=group),
+        )
         polygon_output_name = os.path.join(
-            NEW_TESTS_DIR, POLYGON_OUTPUT_TEMPLATE.substitute(id=test_id_str, group=group))
+            NEW_TESTS_DIR,
+            POLYGON_OUTPUT_TEMPLATE.substitute(id=test_id_str, group=group),
+        )
         cms_input_name = os.path.join(
-            NEW_TESTS_DIR, CMS_INPUT_TEMPLATE.substitute(id=test_id_str, group=group))
+            NEW_TESTS_DIR, CMS_INPUT_TEMPLATE.substitute(id=test_id_str, group=group)
+        )
         cms_output_name = os.path.join(
-            NEW_TESTS_DIR, CMS_OUTPUT_TEMPLATE.substitute(id=test_id_str, group=group))
+            NEW_TESTS_DIR, CMS_OUTPUT_TEMPLATE.substitute(id=test_id_str, group=group)
+        )
 
         os.rename(polygon_input_name, cms_input_name)
         os.rename(polygon_output_name, cms_output_name)
 
-    shutil.make_archive('cms_tests', 'zip', root_dir=NEW_TESTS_DIR)
+    shutil.make_archive("cms_tests", "zip", root_dir=NEW_TESTS_DIR)
 
 
-def get_score_params(groups: List[ET.Element], dependencies: Dict[str, List[str]]) -> str:
+def get_score_params(
+    groups: list[ET.Element], dependencies: dict[str, list[str]]
+) -> str:
     """Returns CMS's Score Parameters string."""
     score_params = []
     for group in groups:
-        name = group.get('name')
-        points = int(float(group.get('points')))
+        name = group.get("name")
+        points = group.get("points")
+        if points is None:
+            points = 0
+        points = int(float(points))
         groups_str = "|".join(dependencies[name])
-        score_params.append(
-            [points, GROUPS_REGEX.substitute(groups=groups_str)])
+        score_params.append([points, GROUPS_REGEX.substitute(groups=groups_str)])
     return json.dumps(score_params)
 
 
@@ -107,17 +116,17 @@ def main():
 
     os.chdir(sys.argv[1])
 
-    tree = ET.parse('problem.xml')
-    groups = tree.find('judging/testset/groups').findall('group')
-    tests = tree.find('judging/testset/tests')
+    tree = ET.parse("problem.xml")
+    groups = tree.find("judging/testset/groups").findall("group")
+    tests = tree.find("judging/testset/tests")
 
     dependencies = parse_dependencies(groups)
     rename_tests(tests)
     score_params = get_score_params(groups, dependencies)
-    with open('score_params.txt', 'w', encoding='UTF-8') as file:
-        file.write(f'{score_params}')
-    print(f'CMS Score Parameters:\n{score_params}')
+    with open("score_params.txt", "w", encoding="UTF-8") as file:
+        file.write(f"{score_params}")
+    print(f"CMS Score Parameters:\n{score_params}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
